cfa1d240f99bbfc1f6d1c452429028c53a76ec33
Димеска Викторија <git@github.com:Ilieska/IS.git>
2024-02-26 09:54:56 +0100

    Add project files.

---
+++ eLibrary/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eLibrary.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string BookURL { get; set; }
+        public string BookArtUrl { get; set; }
+        public decimal Price { get; set; }
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+        public Author Author { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ eLibrary/Models/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace eLibrary.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreID { get; set; }
+        [Required]
+        [DisplayName("GenreName")]
+        public string GenreName { get; set; }
+        public string GenreDesc { get; set; }
+        public virtual List<Book> Books { get; set; }
+
+    }
+}
